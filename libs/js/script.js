27132a10c317c77eeb13e1d3fd4749d8b3977c35
TranPhuongNam <tranphuongnam17041997@gmail.com>
2020-01-04 12:13:26 +0700

    update

---
+++ libs/js/script.js
@@ -0,0 +1,56 @@
+$( document ).ready(function() {
+
+    // js navigation
+    $(".menucate-lv2").hide();
+    $(".rotate-down .menucate-lv2").show();
+    $(".menucate-lv1 > li > a").click(function(){
+        $(".menucate-lv1 > li.hasdrop-cate").removeClass('rotate-down');
+
+        $(".menucate-lv2").slideUp();
+        if(!$(this).next(".menucate-lv2").is(":visible"))
+        {
+            $(this).next(".menucate-lv2").slideDown();
+            $(this).parent().addClass('rotate-down');
+        }
+    });
+});
+$(function() {
+    $(".slider-news").owlCarousel({
+        items: 1,
+        responsive: {
+            1200: { item: 3, },// breakpoint from 1200 up
+            982: { items: 3, },
+            768: { items: 2, },
+            480: { items: 1, },
+            0: { items: 1, }
+        },
+        rewind: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        smartSpeed: 500, //slide speed smooth
+
+        dots: true,
+        dotsEach: false,
+        loop: true,
+        nav: true,
+        navText: ['', ''],
+        // navSpeed: 250, //slide speed when click button
+
+        autoWidth: false,
+        margin: 11,
+
+        lazyContent: false,
+        lazyLoad: false,
+
+        animateIn: false,
+        animateOut: false,
+
+        center: false,
+        URLhashListener: false,
+
+        video: false,
+        videoHeight: false,
+        videoWidth: false,
+    });
+});
