592a6b9ae389fe8fe63699a0f1ce969f6415ea15
TranPhuongNam <tranphuongnam17041997@gmail.com>
2020-01-08 17:53:25 +0700

    update

--- libs/js/script.js
+++ libs/js/script.js
@@ -32,7 +32,32 @@ $( document ).ready(function() {
         $(".btn-bars-head button").trigger('click');
     });
 
+    // js nhat ky
+    $('.bars-close').click(function () {
+        $('.col-vsd-25').toggleClass('change-width');
+        $('.col-vsd-75').toggleClass('change-width');
+    });
+    $('.bars-close').hover(function () {
+        $('.heading-diary').toggleClass('change-color');
+    });
 
+    // js upload file
+    var fileInput  = document.querySelector( ".input-file" ),
+        button     = document.querySelector( ".input-file-trigger" ),
+        the_return = document.querySelector(".file-return");
+
+    button.addEventListener( "keydown", function( event ) {
+        if ( event.keyCode == 13 || event.keyCode == 32 ) {
+            fileInput.focus();
+        }
+    });
+    button.addEventListener( "click", function( event ) {
+        fileInput.focus();
+        return false;
+    });
+    fileInput.addEventListener( "change", function( event ) {
+        the_return.innerHTML = this.value;
+    });
 
 });
 $(function() {
